b94bd02cfb3193dd404c68270e99ec266a98a58b
umutsoyyilmaz <umut.soyyilmaz@gmail.com>
2026-02-02 11:58:51 +0000

    ci(e2e): make nav interactions robust by using app nav and explicit tab clicks

--- cypress/e2e/basic.spec.js
+++ cypress/e2e/basic.spec.js
@@ -6,9 +6,10 @@ describe('Basic UI & accessibility checks', () => {
     cy.visit('/');
     cy.title().should('include', 'AI Project Co-Pilot');
 
-    // Make sure Projects view is active then open New Project modal
-    cy.contains('Projects').click({ force: true });
-    cy.contains('+ New Project').click();
+    // Activate Projects view via the app nav and open New Project modal
+    cy.window().then(win => win.navTo('projects'));
+    cy.get('#view-projects').should('be.visible');
+    cy.get('#view-projects').contains('+ New Project').click();
     cy.get('[role="dialog"]').should('be.visible');
     cy.get('[role="dialog"]').should('have.attr', 'aria-modal', 'true');
 
@@ -19,18 +20,24 @@ describe('Basic UI & accessibility checks', () => {
 
   it('prevents XSS in document chat (sanitizes input)', () => {
     cy.visit('/');
-    // Ensure Design view is active before trying to open a document
-    cy.contains('Design').click({ force: true });
+    // Activate Design view via the app nav
+    cy.window().then(win => win.navTo('design'));
+    cy.get('#view-design').should('be.visible');
 
     // Open document detail if there is one
-    cy.get('table').then($tbl => {
+    cy.get('#view-design table').then($tbl => {
       // try to open first document row
       if($tbl.find('tbody tr').length) {
-        cy.get('tbody tr').first().click({ force: true });
-        cy.get('#docChatInput').should('exist');
+        cy.get('#view-design tbody tr').first().click({ force: true });
+
+        // Switch to Co-Pilot tab inside document detail and ensure chat input is visible
+        cy.get('#view-document-detail').contains('Co-Pilot').click();
+        cy.get('#docChatInput').should('be.visible');
+
         const payload = '<img src=x onerror=alert(1)>DROP';
         cy.get('#docChatInput').type(payload);
-        cy.contains('Send').click({ force: true });
+        cy.get('#view-document-detail').contains('Send').click({ force: true });
+
         // ensure that chat area does not contain unescaped onerror or injected tags and contains payload as text
         cy.get('#docChatArea').invoke('html').should('not.contain', 'onerror').and('not.contain', '<img').and('not.contain', '<script');
         cy.get('#docChatArea').should('contain.text', 'DROP');
